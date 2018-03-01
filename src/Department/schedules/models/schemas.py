db12409a873e4355be5f602b45a0aa03e7720ff9
Jamel Travis <jtravis1@york.cuny.edu>
2018-03-01 22:12:26 +0000

    completed skeleton for main model, completed skeleton for vocabularies

--- src/Department/schedules/models/schemas.py
+++ src/Department/schedules/models/schemas.py
@@ -2,16 +2,63 @@
 """Module for Zope Schema models used for Department.schedules"""
 
 from Department.courses import _
-from Department.courses.resources import vocabu
-from plone.autoform import model
+from Department.courses.resources import vocabulary
+from plone.autoform import model, directives
+from plone import api
+from z3c.form.browser.checkbox import CheckBoxFieldWidget as checkboxes
 from zope import schema
 
 class ICourses(model.Schema):
     """Schema for entering courses."""
 
-    department = schema.Choice(
-        title=(u'Department'),
-        values=
-
+    subject = schema.Choice(
+        title=(u'Course Subject'),
+        values=[],
     )
-    
+
+    courseNumber = schema.Choice(
+        title=(u'Course Number'),
+        values=[],
+    )
+
+    courseSection = schema.TextLine(
+        title=(u'Section'),
+        values=[],
+    )
+
+    # Maybe this should be ommitted?
+    enrollmentCapacity = schema.Int(
+        title=(u'Enrollment Capacity'),
+        max=200,
+    )
+
+    waitList = schema.Int(
+        title=(u'Wait List'),
+        max=200,
+    )
+
+    component = schema.Choice(
+        title=(u'Course Component'),
+        values=[],
+    )
+
+    directives.widget(classDays=checkboxes)
+    courseDays = schema.List(
+        title=(u'Days'),
+        values=[],
+    )
+
+    courseTimes = schema.Timedelta(
+        title=(u'Times'),
+    )
+
+    directives.widget(classDays=checkboxes)
+    attributes = schema.List(
+        title=(u'attributes'),
+        values=[],
+    )
+
+    instructor = schema.Choice(
+        title=(u'Course Instructor'),
+        values=[],
+    )
--- src/Department/schedules/resources/vocabulary.py
+++ src/Department/schedules/resources/vocabulary.py
@@ -2,5 +2,35 @@
 """Vacubularies used for choice fields"""
 
 from Department.courses import _
+from plone import api
 
-FACULTY = 
+# Stock Faculty vocabularies
+BEHAVIOR_FACULTY = ['Robert Duncan', 'Robin Harper']
+SOCIOLOGY = ['foo', 'bar', 'spam']
+
+def extend_faculty(department: str, vocabulary: list) -> list:
+    """Extends list of instructors if necessary.
+
+    Searches the catalog for new instructor names, if new instructors are present,
+    extend method will be called. Strings are made unicode strings as last step.
+
+    Args:
+        department (str): name of the department (see Department.courses.add_faculty)
+        vocabulary (list): list containing department specific faculty names
+
+    Returns:
+        list: ['Professor Foo, Professor Bar']
+    
+    Example:
+        >>>extend_faculty('Behavioral Science', BEHAVIOR_FACULTY)
+        [u'Robert Duncan', u'Robin Harper', u'Kirstin Davies']
+    """
+    catalog = api.portal.get_tool('portal_catalog')
+    findFaculty = catalog.searchResults(**{
+        'portal_type': 'AddFaculty',
+        'department': department
+        })
+    results = [item for item in findFaculty if item['facultyName'] not in vocabulary]
+    if not results:
+        return sorted(map(unicode, vocabulary))
+    return sorted(map(unicode, vocabulary.extend(results)))
