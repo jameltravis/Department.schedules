7d87da3253e8301ffa0203753f03fdb2ef26cb2d
Jamel Travis <jtravis1@york.cuny.edu>
2018-03-08 21:51:54 +0000

    added structure for courses vocabulary

--- src/Department/schedules/resources/vocab_source.py
+++ src/Department/schedules/resources/vocab_source.py
@@ -6,7 +6,7 @@ from Department.courses import _
 # Behavioral Science Full time faculty
 DETAILED_FACULTY = [
     {
-        'department': u"Behavioral Sciences",
+        'department': u"Behavioral_Sciences",
         'facultyName': u"Robert Duncan",
         'titleRank': u"Associate Professor",
         'tenure': u"Yes",
@@ -16,3 +16,25 @@ DETAILED_FACULTY = [
 # need to decide if we should filter the different dictionary keys
 FACULTY = [item['facultyName'] for item in DETAILED_FACULTY]
 
+# Basic idea is this - 'department': ["subject1", "subject2", "subject3"]
+DEPARTMENT_SUBJECTS = [
+    {
+        'Behavioral_Sciences': [],
+        'Biology': [],
+        'chemistry':[],
+        'physics': [],
+        'English': [],
+        'history': [],
+        'math': [],
+        'fine_arts': [],
+        'world_languages': [],
+        'accounting_finance': [],
+        'business_economics': [],
+        'physical_education': [],
+        'health_professions': [],
+        'nursing': [],
+        'occupational_therapy': [],
+        'social_work': [],
+        'teacher_education': [],
+    }
+]
--- src/Department/schedules/resources/vocabulary.py
+++ src/Department/schedules/resources/vocabulary.py
@@ -5,11 +5,19 @@ from zope.interface import implements
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary
 from plone import api
+from plone.momoize import ram
 from Department.courses import _
 from Department.courses.resources.vocab_source import FACULTY
 
 
+def __catalog_count_cachekey():
+    """Returns a cache key based on catalog updates"""
 
+    catalog = api.portal.get_tool('portal_catalog')
+    return str(catalog.getCounter())
+
+
+@ram.cache(__catalog_count_cachekey)
 def extend_faculty(department: str, vocabulary: list) -> list:
     """Extends list of instructors if necessary.
 
@@ -62,6 +70,7 @@ class GetFaculty(object):
 
     implements(IContextSourceBinder)
     
+    @ram.cache(__catalog_count_cachekey)
     def __call__(self, context, vocabulary):
         vocabulary = [item['name'] for item in FACULTY if item['department'] == context]
         catalog = api.portal.get_tool('portal_catalog')
@@ -76,3 +85,45 @@ class GetFaculty(object):
         if not results:
             return SimpleVocabulary.fromValues(sorted(map(unicode, vocabulary)))
         return SimpleVocabulary.fromValues(sorted(map(unicode, vocabulary.extend(results))))
+
+
+class GetCourseSubjects(object):
+    """Source for schema.Choice fields.
+    
+    Provides a list of faculty members and makes them a vocabulary.
+    Searches the catalog for new instructor names, if new instructors are present,
+    extend method will be called. Strings converted to unicode (this is Python 2,
+    after all) then passed to SimpleVocabulary.fromValues .
+
+    Args:
+        none.
+
+    Returns:
+        list: ['Professor Foo, Professor Bar']
+    
+    Example:
+        >>>foo = schema.Choice(
+            title=(u'List of Faculty Names'),
+            source=GetFaculty,
+        )
+        [u'Robert Duncan', u'Robin Harper', u'Kirstin Davies']
+    """
+
+    implements(IContextSourceBinder)
+    
+    @ram.cache(__catalog_count_cachekey)
+    def __call__(self, context, vocabulary):
+        vocabulary = [item['name'] for item in FACULTY if item['department'] == context]
+        catalog = api.portal.get_tool('portal_catalog')
+        findFaculty = catalog.searchResults(**{
+            'portal_type': 'AddFaculty',
+            'department': context
+            })
+        results = [
+            item['facultyName'] for item in findFaculty 
+            if item['facultyName'] not in vocabulary
+            ]
+        if not results:
+            return SimpleVocabulary.fromValues(sorted(map(unicode, vocabulary)))
+        return SimpleVocabulary.fromValues(sorted(map(unicode, vocabulary.extend(results))))
+
