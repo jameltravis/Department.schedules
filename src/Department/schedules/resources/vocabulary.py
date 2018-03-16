3a796ef60c1fcaedfe2f044cff997b5fc96fb2d9
Jamel Travis <jtravis1@york.cuny.edu>
2018-03-16 14:34:20 +0000

    removed cache key definitions

--- src/Department/schedules/resources/vocabulary.py
+++ src/Department/schedules/resources/vocabulary.py
@@ -20,31 +20,6 @@ from Department.schedules.resources.vocab_source import (
     COURSE_COMPONENTS
     )
 
-# Cache Keys
-
-def __catalog_count_cachekey():
-    """Returns a cache key based on catalog updates"""
-
-    catalog = api.portal.get_tool('portal_catalog')
-    return str(catalog.getCounter())
-
-def __course_component_cachekey():
-    """Returns number of AddCourse content types in the catalog as a string.
-
-    Args:
-        None.
-    
-    Returns:
-        str: 'this is a string'
-
-    Example:
-        >>>__course_component_cachekey()
-        '9420'
-    """
-    catalog = api.portal.get_tool('portal_catalog')
-    results = catalog.searchResults(**{'portal_type': 'AddCourse'})
-    return str(len(results))
-
 
 # Vocabularies below
 
