c9a94377fb8b2447013235f343af9e2f98032446
Jamel Travis <jtravis1@york.cuny.edu>
2018-03-23 13:45:09 +0000

    added class inheriting from 'Persistent', lets see if this works

--- src/Department/schedules/schemas/schedule.py
+++ src/Department/schedules/schemas/schedule.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 """Module used for adding new courses"""
 
+# Model/Schema Interface
 from zope import schema
+from zope.interface import implements
 from plone.directives import form
 from plone.supermodel import model
 from collective.z3cform.datagridfield import DictRow
 from collective.z3cform.datagridfield.datagridfield import DataGridFieldFactory
 from Department.schedules import _
 from Department.schedules.models.schemas import WEEKDAY_SCHEMA, WEEKEND_SCHEMA
+
+# Make the model/schema Persistent
+from persistent import Persistent
+from zope.schema.fieldproperty import FieldProperty
 
 
 class ISchedule(model.schema):
@@ -19,7 +25,7 @@ class ISchedule(model.schema):
     )
 
     # Data grid for Daytime Course requests
-    form.widget(newCourses=DataGridFieldFactory)
+    form.widget(dayCourses=DataGridFieldFactory)
     dayCourses = schema.List(
         title=(u'Please submit your requests for daytime courses'),
         value_type=DictRow(
@@ -30,7 +36,7 @@ class ISchedule(model.schema):
     )
 
     # Data grid for evening Course requests
-    form.widget(newCourses=DataGridFieldFactory)
+    form.widget(eveningCourses=DataGridFieldFactory)
     eveningCourses = schema.List(
         title=(u'Please submit your requests for daytime courses'),
         value_type=DictRow(
@@ -41,7 +47,7 @@ class ISchedule(model.schema):
     )
 
     # Weekend Datagrid
-    form.widget(newCourses=DataGridFieldFactory)
+    form.widget(weekendCourses=DataGridFieldFactory)
     weekendCourses = schema.List(
         title=(u'Please submit your requests for daytime courses'),
         value_type=DictRow(
@@ -50,3 +56,15 @@ class ISchedule(model.schema):
         ),
         required=True,
     )
+
+
+class Schedule(Persistent):
+    """Store contents from the schedule."""
+
+    implements(ISchedule)
+
+    title = FieldProperty(ISchedule['title'])
+    dayCourses = FieldProperty(ISchedule['dayCourses'])
+    eveningCourses = FieldProperty(ISchedule['EveningCourses'])
+    weekendCourses = FieldProperty(ISchedule['weekendCourses'])
+
