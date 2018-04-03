9749fe03a67a8b532283972d98b0a45c73438b5d
Jamel Travis <jtravis1@york.cuny.edu>
2018-04-03 14:42:25 +0000

    added a class for future schedule grids

--- src/Department/schedules/interfaces/datagrid_schemas.py
+++ src/Department/schedules/interfaces/datagrid_schemas.py
@@ -15,9 +15,84 @@ from z3c.form.browser.checkbox import CheckBoxFieldWidget as checkboxes
 #     WEEKEND,
 # )
 
-
 # ICourses is used to create intances for Weekday and
 # weekend datagrids
+
+class MainTestGrid(model.schema):
+    """Schema to end all schemas."""
+
+    def __init__(self, title):
+        """Init test datagrid.
+        """
+        self.title = title
+
+        self.subject = schema.Choice(
+            title=(u'Course Subject'),
+            values=[u'TEST',u'CourseSubjectVocab'],
+            required=False,
+        )
+
+        self.courseNumber = schema.TextLine(
+            title=(u'Course Number'),
+            required=False,
+        )
+
+        self.courseSection = schema.TextLine(
+            title=(u'Section'),
+            required=False,
+        )
+
+        self.enrollmentCapacity = schema.TextLine(
+            title=(u'Enrollment Capacity'),
+            required=False,
+        )
+
+        self.waitlist = schema.Int(
+            title=(u'Wait List'),
+            required=False,
+            max=500,
+        )
+
+        self.courseDays = schema.TextLine(
+            title(u'Days'),
+            required=False,
+            max=500,
+        )
+
+        self.timeStart = schema.Choice(
+            title=(u'Days'),
+            required=False,
+            values=[u'Hours', u'mins', u'secs'],
+        )
+
+        self.timeEnd = schema.Choice(
+            title=(u'Time End'),
+            required=False,
+            values=[u'Hours', u'mins', u'secs'],
+        )
+
+        self.component = schema.TextLine(
+            title=(u'Course Component(s)'),
+            default=(u"thing1, thing2, thing3"),
+            required=False,
+        )
+
+        directives.widget(attributes=checkboxes)
+        self.attributes = schema.List(
+            title=(u'attributes'),
+            required=False,
+            value_type=schema.Choice(
+                values=[u'value1', u'value2', u'value3' u'None'],
+                default=(u'None')
+            ),
+        )
+
+        self.instructor = schema.Choice(
+            title=(u'Course instructor'),
+            required=False,
+            values=[u'instructor1', u'instructor2'],
+        )
+
 
 class ICourses(model.Schema):
     """Schema for main course datagrid."""
