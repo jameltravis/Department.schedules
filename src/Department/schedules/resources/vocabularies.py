4479f00471c5b4f3974c4c1e1f2ff2e54a791e09
Jamel Travis <jtravis1@york.cuny.edu>
2018-04-07 06:30:18 +0000

    created what I hope is the faculty vocabulary

--- src/Department/schedules/resources/vocabularies.py
+++ src/Department/schedules/resources/vocabularies.py
@@ -2,19 +2,22 @@
 """Module containing vocabularies called from control panel."""
 
 from plone import api
-from plone.app.vocabularies.terms import safe_simplevocabulary_from_values
+from plone.app.vocabularies.terms import (
+    safe_simplevocabulary_from_values as safe_vocab)
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 
 
 
 
-
+# Get ranks from registry
 @provider(IVocabularyFactory)
 def RankVocabularyFactory(context):
     values = api.portal.get_registry_record('york.scheduling.newTitle')
-    return safe_simplevocabulary_from_values(values)
+    return safe_vocab(values)
 
+
+# Get the course subjects from registry
 @provider(IVocabularyFactory)
 def subject_vocabulary_factory(context):
     """Provides dropdowns for the course subejcts.
@@ -40,21 +43,29 @@ def subject_vocabulary_factory(context):
         )
     """
     # get current user
-    current_user = api.user.get_current()
-    user_dept = current_user.getProperty('department')
+    currentUser = api.user.get_current()
+    userDept = currentUser.getProperty('department')
     records = api.portal.get_registry_record('york.scheduling.courseSubjects')
     
     # get rid of everything before semicolon
     values = tuple(
         [item.split(': ')[1].upper() for item in records 
-        if user_dept.lower() in item.lower()]
+        if userDept.lower() in item.lower()]
     )
-    return safe_simplevocabulary_from_values(values)
+    return safe_vocab(values)
 
 
-
-
-# @provider(IVocabularyFactory)
-# def DepartmentVocabularyFactory(context):
-#     current_user = api.user.get_current()
-#     values = api.portal.get_registry_record('york.scheduling.department')
+# get faculty from portal
+@provider(IVocabularyFactory)
+def FacultyVocabularyFactory(context):
+    """insert good docstring here.
+    """
+    #TODO Add better docstring!
+    currentUser = api.user.get_current()
+    crntUserDept = currentUser.getProperty('department')
+    users = api.user.get_users()
+    values = tuple(
+        [user.upper() for user in users 
+        if user.getProperty('department').lower() == crntUserDept.lower()]
+    )
+    return safe_vocab(values)
